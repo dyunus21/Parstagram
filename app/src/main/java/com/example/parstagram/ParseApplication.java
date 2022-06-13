8af8318953532f6bd1c6a1b53179b33d45db4407
Diya Yunus <diyayunus@fb.com>
2022-06-13 14:34:48 -0700

    Connected to Parse Database

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,12 +3,14 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("tsbTun3egsM7QmBHueFFi5zx7pGjUE8xaIXpaNSo")
                 .clientKey("oAdLuKSSpkWWOj7xVXSXNOV8lwmSEs8CYUJjJuc5")
