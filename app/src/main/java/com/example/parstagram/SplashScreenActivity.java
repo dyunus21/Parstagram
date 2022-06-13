10a22484d9d89e3b1226f9eaffcb646677267495
Diya Yunus <diyayunus@fb.com>
2022-06-13 15:21:38 -0700

    Finished implementing SplashScreenActivity

--- app/src/main/java/com/example/parstagram/SplashScreenActivity.java
+++ app/src/main/java/com/example/parstagram/SplashScreenActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 700);
     }
 }
